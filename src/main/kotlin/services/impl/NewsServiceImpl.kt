651f91d2e2918431a6ffde1c2b47448df7bfaf4a
Artem Rogowski <pavetranyshar@mail.ru>
2020-12-22 16:04:53 +0300

    Impl News endpoints

--- src/main/kotlin/services/impl/NewsServiceImpl.kt
+++ src/main/kotlin/services/impl/NewsServiceImpl.kt
@@ -1,6 +1,30 @@
 package services.impl
 
+import controllers.models.NewsModel
+import dao.NewsDao
+import dao.models.NewsDaoModel
 import services.NewsService
+import java.util.*
 
-class NewsServiceImpl : NewsService {
+class NewsServiceImpl(private val newsDao: NewsDao) : NewsService {
+    override suspend fun getAllNews(): List<NewsDaoModel> {
+        return newsDao.getAllNews()
+    }
+
+    override suspend fun getNewsItem(newsId: UUID): NewsDaoModel {
+        return newsDao.getNewsItem(newsId)
+    }
+
+    override suspend fun addNewsItem(newsModel: NewsModel): UUID {
+        return newsDao.addNewsItem(newsModel)
+    }
+
+    override suspend fun updateNewsItem(newsId: UUID, updatedNews: NewsModel) {
+        newsDao.updateNewsItem(newsId, updatedNews)
+    }
+
+    override suspend fun deleteNewsItem(newsId: UUID) {
+        newsDao.deleteNewsItem(newsId)
+    }
+
 }
